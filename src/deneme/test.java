bb68787a86983ff2e7461f4e23ceabe9e263cc2f
ASUS <ASUS@ASUS.mshome.net>
2024-09-26 19:10:04 +0300

    for each kullanımı

--- src/deneme/test.java
+++ src/deneme/test.java
@@ -4,7 +4,15 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World! Sado");
-
+		//for each kullanımı
+		String [] yazilimDilleri = {
+				"java",
+				"python",
+				"C"
+		};
+		for (String yazilimdili : yazilimDilleri) {
+			System.out.println(yazilimdili);
+		}
 	}
 
 }
